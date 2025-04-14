f4b6b7787bca168b8f1140f741e0ad9908959bda
estillicon <paugracialopez86@yahoo.com>
2025-04-14 21:30:54 +0200

    gif

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react';
 import './App.css';
 import BookList from './components/BookList';
 import './styles.css';
+import gifBanner from './assets/gifP3.gif';
 
 function App() {
   return (
     <div className="App">
       <BookList />
+      <a href="https://www.iesesteveterradas.cat/" target="_blank" rel="noopener noreferrer">
+        <img src={gifBanner} alt="GIF de final de página" className="gif-banner" />
+      </a>
     </div>
   );
 }
