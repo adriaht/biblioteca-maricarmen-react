d413ab705acf886baad482d88de91f56f46d61fe
adriaht <adriah.t.22@gmail.com>
2025-04-24 18:06:01 +0200

    ajustes de estilos

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -34,7 +34,7 @@ function BookDetails({ bookId, onBack, extraProp }) {
 
         // 3. Filtrar els exemplars relacionats amb aquest llibre:
         // Assumim que cada exemplar té una propietat "cataleg" amb un camp "id"
-        const bookExemplars = exemplarsData.filter(exemplar => 
+        const bookExemplars = exemplarsData.filter(exemplar =>
           exemplar.cataleg?.id === bookData.id
         );
         console.log("Exemplars filtrats per aquest llibre:", JSON.stringify(bookExemplars, null, 2));
@@ -92,93 +92,95 @@ function BookDetails({ bookId, onBack, extraProp }) {
   }
 
   return (
-    <div className="book-details-container">
-      <button onClick={onBack} className="back-btn">← Tornar a la llista</button>
-      <div className="book-details-card">
-        <h2 className="details-title h2">Detalls del llibre</h2>
-        {loading ? (
-          <div className="loading-container">
-            <div className="loading-spinner"></div>
-            <p>Cargant detalls...</p>
-          </div>
-        ) : error ? (
-          <p className="error-message">{error}</p>
-        ) : book ? (
-          <div className="book-info-details">
-            <h3 className="book-title-details h3">{book.titol}</h3>
-            <div className="book-metadata">
-              <div className="metadata-item"><span>ID:</span> {book.id}</div>
-              <div className="metadata-item"><span>Autor:</span> {book.autor || "No especificat"}</div>
-              {book.editorial && <div className="metadata-item"><span>Editorial:</span> {book.editorial}</div>}
-              {book.ISBN && <div className="metadata-item"><span>ISBN:</span> {book.ISBN}</div>}
-              {book.titol_original && <div className="metadata-item"><span>Títol original:</span> {book.titol_original}</div>}
-              {book.colleccio && <div className="metadata-item"><span>Colecció:</span> {book.colleccio}</div>}
-              {book.data_edicio && (
-                <div className="metadata-item">
-                  <span>Fecha de edición:</span> {new Date(book.data_edicio).toLocaleDateString()}
+    <div className="container">
+
+        <button onClick={onBack} className="back-btn">← Tornar a la llista</button>
+        <div className="book-details-card">
+          <h2 className="details-title h2">Detalls del llibre</h2>
+          {loading ? (
+            <div className="loading-container">
+              <div className="loading-spinner"></div>
+              <p>Cargant detalls...</p>
+            </div>
+          ) : error ? (
+            <p className="error-message">{error}</p>
+          ) : book ? (
+            <div className="book-info-details">
+              <h3 className="book-title-details h3">{book.titol}</h3>
+              <div className="book-metadata">
+                <div className="metadata-item"><span>ID:</span> {book.id}</div>
+                <div className="metadata-item"><span>Autor:</span> {book.autor || "No especificat"}</div>
+                {book.editorial && <div className="metadata-item"><span>Editorial:</span> {book.editorial}</div>}
+                {book.ISBN && <div className="metadata-item"><span>ISBN:</span> {book.ISBN}</div>}
+                {book.titol_original && <div className="metadata-item"><span>Títol original:</span> {book.titol_original}</div>}
+                {book.colleccio && <div className="metadata-item"><span>Colecció:</span> {book.colleccio}</div>}
+                {book.data_edicio && (
+                  <div className="metadata-item">
+                    <span>Fecha de edición:</span> {new Date(book.data_edicio).toLocaleDateString()}
+                  </div>
+                )}
+                {book.pagines && <div className="metadata-item"><span>Páginas:</span> {book.pagines}</div>}
+                {book.llengua?.nom && <div className="metadata-item"><span>Idioma:</span> {book.llengua.nom}</div>}
+                {book.pais?.nom && <div className="metadata-item"><span>País:</span> {book.pais.nom}</div>}
+              </div>
+              {book.resum && (
+                <div className="book-description">
+                  <h4>Resum:</h4>
+                  <p>{book.resum}</p>
                 </div>
               )}
-              {book.pagines && <div className="metadata-item"><span>Páginas:</span> {book.pagines}</div>}
-              {book.llengua?.nom && <div className="metadata-item"><span>Idioma:</span> {book.llengua.nom}</div>}
-              {book.pais?.nom && <div className="metadata-item"><span>País:</span> {book.pais.nom}</div>}
-            </div>
-            {book.resum && (
-              <div className="book-description">
-                <h4>Resum:</h4>
-                <p>{book.resum}</p>
-              </div>
-            )}
-            {book.anotacions && (
-              <div className="book-notes">
-                <h4>Anotacions:</h4>
-                <p>{book.anotacions}</p>
-              </div>
-            )}
-            {book.thumbnail_url && (
-              <div className="book-cover">
-                <img src={book.thumbnail_url} alt={`Portada de ${book.titol}`} />
-              </div>
-            )}
-            {book.info_url && (
-              <div className="book-links">
-                <a href={book.info_url} target="_blank" rel="noopener noreferrer" className="external-link">
-                  Més informació
-                </a>
-              </div>
-            )}
-            
-            {/* Secció extra: Mostrem els totals i detalls per centre */}
-            <div className="extra-info">
-              {/* <h4>Detalls dels exemplars</h4> */}
-              {/* <p>Total exemplars (sense baixa): {totalExemplars}</p>
-              <p>Exclosos: {totalExclosos} | No exclosos: {totalNoExclosos}</p> */}
-              
-              <h5>Exemplars disponiblen en cada centre:</h5>
-              {Object.keys(centreStats).length > 0 ? (
-               <ul>
-               {Object.entries(centreStats).map(([centre, stats]) => (
-                 <li key={centre}>
-                   {centre}:{' '}
-                   <span style={{ color: 'red' }}>
-                     Exclosos: {stats.exclosos}
-                   </span>{' '}
-                   |{' '}
-                   <span style={{ color: 'green' }}>
-                     No exclosos: {stats.noExclosos}
-                   </span>
-                 </li>
-               ))}
-             </ul>
-              ) : (
-                <p>No hi ha exemplars disponibles per cap centre.</p>
+              {book.anotacions && (
+                <div className="book-notes">
+                  <h4>Anotacions:</h4>
+                  <p>{book.anotacions}</p>
+                </div>
               )}
-              {extraProp && <p className="extra-prop">Prop extra: {extraProp}</p>}
+              {book.thumbnail_url && (
+                <div className="book-cover">
+                  <img src={book.thumbnail_url} alt={`Portada de ${book.titol}`} />
+                </div>
+              )}
+              {book.info_url && (
+                <div className="book-links">
+                  <a href={book.info_url} target="_blank" rel="noopener noreferrer" className="external-link">
+                    Més informació
+                  </a>
+                </div>
+              )}
+
+              {/* Secció extra: Mostrem els totals i detalls per centre */}
+              <div className="extra-info">
+                {/* <h4>Detalls dels exemplars</h4> */}
+                {/* <p>Total exemplars (sense baixa): {totalExemplars}</p>
+              <p>Exclosos: {totalExclosos} | No exclosos: {totalNoExclosos}</p> */}
+
+                <h5>Exemplars disponiblen en cada centre:</h5>
+                {Object.keys(centreStats).length > 0 ? (
+                  <ul>
+                    {Object.entries(centreStats).map(([centre, stats]) => (
+                      <li key={centre}>
+                        {centre}:{' '}
+                        <span style={{ color: 'red' }}>
+                          Exclosos: {stats.exclosos}
+                        </span>{' '}
+                        |{' '}
+                        <span style={{ color: 'green' }}>
+                          No exclosos: {stats.noExclosos}
+                        </span>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No hi ha exemplars disponibles per cap centre.</p>
+                )}
+                {extraProp && <p className="extra-prop">Prop extra: {extraProp}</p>}
+              </div>
             </div>
-          </div>
-        ) : (
-          <p className="error-message">No s'ha trobat informació per a aquest llibre.</p>
-        )}
-      </div>
+          ) : (
+            <p className="error-message">No s'ha trobat informació per a aquest llibre.</p>
+          )}
+        </div>
+    
     </div>
   );
 }
--- src/components/CsvUpload.jsx
+++ src/components/CsvUpload.jsx
@@ -50,57 +50,68 @@ const CsvUpload = () => {
   };
 
   return (
-    <div className="p-6 max-w-md mx-auto bg-gray-200 rounded-lg shadow-md border-2 border-gray-300">
-      <h2 className="text-black text-3xl font-semibold text-center mb-4">
-        Subir CSV
-      </h2>
+    <div className="container">
+      <div className="p-6 max-w-md mx-auto bg-gray-200 rounded-lg shadow-md border-2 border-gray-300"
+          style={{
+            backgroundColor: "var(--card-bg)",
+            borderColor: "var(--border-color)",
+          }}>
+        <h2 className="text-black text-3xl font-semibold text-center mb-4"
+          style={{ color: "var(--text-color)" }}>
+          Subir CSV
+        </h2>
 
-      <div className="mb-4 flex w-full flex-col justify-center">
-        <input
-          type="file"
-          accept=".csv"
-          onChange={handleFileChange}
-          className="bg-[#DDE1F1] text-black truncate py-2 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-[#214093] focus:ring-opacity-50 cursor-pointer block "
-        />
-      </div>
-
-      <div className="flex justify-around">
-        <button
-          onClick={handleUpload}
-          className="bg-[#214093] text-white px-4 py-2 rounded-md border-none cursor-pointer shadow-md transition-all duration-100 ease-in-out active:bg-[#1A3379] active:translate-y-[1px] focus:outline-none"
-          style={{ backgroundColor: "#214093" }}
-        >
-          {isLoading ? "Subiendo..." : "Subir Archivo"}
-        </button>
-        <button
-          onClick={() => {
-            setFile(null); // Limpiar el archivo
-            setMessage("");
-            setErrores([]);
-          }}
-          className="bg-[#214093] text-white px-4 py-2 rounded-md border-none cursor-pointer shadow-md transition-all duration-100 ease-in-out active:bg-[#1A3379] active:translate-y-[1px] focus:outline-none"
-          style={{ backgroundColor: "#214093" }}
-        >
-          Cancelar
-        </button>
-      </div>
-
-      <p className="mt-4 text-black whitespace-pre-wrap">{message}</p>
-
-      {errores.length > 0 && (
-        <div className="mt-4 bg-red-100 border border-red-400 text-red-700 p-4 rounded-lg max-h-80 overflow-y-auto">
-          <h3 className="font-semibold mb-2">Registros no procesados:</h3>
-          <ul className="list-disc pl-5 text-sm space-y-3">
-            {errores.map((err, index) => (
-              <li key={index}>
-                <div className="mb-1 font-medium">
-                  ({index + 1}) Email: {err.fila.email} — Motivo: {err.error}
-                </div>
-              </li>
-            ))}
-          </ul>
+        <div className="mb-4 flex w-full flex-col justify-center">
+          <input
+            type="file"
+            accept=".csv"
+            onChange={handleFileChange}
+            className="bg-[#DDE1F1] text-black truncate py-2 rounded-md border-none focus:outline-none focus:ring-2 focus:ring-[#214093] focus:ring-opacity-50 cursor-pointer block "
+          />
         </div>
-      )}
+
+        <div className="flex justify-around">
+          <button
+            onClick={handleUpload}
+            className="bg-[#214093] text-white px-4 py-2 rounded-md border-none cursor-pointer shadow-md transition-all duration-100 ease-in-out active:bg-[#1A3379] active:translate-y-[1px] focus:outline-none"
+            style={{
+              backgroundColor: "var(--button-bg)",
+            }}
+          >
+            {isLoading ? "Subiendo..." : "Subir Archivo"}
+          </button>
+          <button
+            onClick={() => {
+              setFile(null); // Limpiar el archivo
+              setMessage("");
+              setErrores([]);
+            }}
+            className="bg-[#214093] text-white px-4 py-2 rounded-md border-none cursor-pointer shadow-md transition-all duration-100 ease-in-out active:bg-[#1A3379] active:translate-y-[1px] focus:outline-none"
+            style={{
+              backgroundColor: "var(--button-bg)",
+            }}
+          >
+            Cancelar
+          </button>
+        </div>
+
+        <p className="mt-4 text-black whitespace-pre-wrap">{message}</p>
+
+        {errores.length > 0 && (
+          <div className="mt-4 bg-red-100 border border-red-400 text-red-700 p-4 rounded-lg max-h-80 overflow-y-auto">
+            <h3 className="font-semibold mb-2">Registros no procesados:</h3>
+            <ul className="list-disc pl-5 text-sm space-y-3">
+              {errores.map((err, index) => (
+                <li key={index}>
+                  <div className="mb-1 font-medium">
+                    ({index + 1}) Email: {err.fila.email} — Motivo: {err.error}
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
